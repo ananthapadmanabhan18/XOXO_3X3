196fe8a332333b0d1ed9dd99449def0cfcb5c148
Anantha Padmanabhan M Nair <apm.nair@niser.ac.in>
2024-05-18 16:58:56 +0530

    Add project files.

---
+++ XOXO_3X3/XOXO_3X3.cpp
@@ -0,0 +1,12 @@
+﻿// XOXO_3X3.cpp : Defines the entry point for the application.
+//
+
+#include "XOXO_3X3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
